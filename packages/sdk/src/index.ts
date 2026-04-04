d9fb48f0c07c436524b0a15988d41e525b688146
UmbertoToraldo <umbertoraldo@gmail.com>
2026-04-04 18:22:35 +0200

    Create index.ts to re-export SDK modules
    Re-export all modules from tree, anchor, verify, and types.

---
+++ packages/sdk/src/index.ts
@@ -0,0 +1,8 @@
+│           │   # COSA: re-export di tutto. Il punto di ingresso.
+│           │   # COME: export * from './tree'
+│           │   #       export * from './anchor'
+│           │   #       export * from './verify'
+│           │   #       export * from './types'
+│           │   # PERCHÉ: chi importa l'SDK fa:
+│           │   #   import { buildTree, anchorToXRPL } from 'xrpl-merkle-sdk'
+│           │   # QUANDO: prima dell'hackathon.
